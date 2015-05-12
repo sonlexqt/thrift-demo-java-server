88e22c4ed8ced94e5cb213093358f67b1d7a31d2
thaisonle94 <thaisonle94@gmail.com>
2015-05-12 16:23:14 +0700

    pushes all java server code at the present

---
+++ src/APIsHandler.java
@@ -0,0 +1,32 @@
+
+import org.apache.thrift.TException;
+import thriftDemo.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sonle
+ */
+public class APIsHandler implements APIs.Iface {
+
+    @Override
+    public void put() throws TException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void increase(String _username) throws TException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int get(String _username) throws TException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/JavaServer.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sonle
+ */
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TServer.Args;
+import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.TSSLTransportFactory;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TServerTransport;
+import org.apache.thrift.transport.TSSLTransportFactory.TSSLTransportParameters;
+import thriftDemo.*;
+
+public class JavaServer {
+
+    public static APIsHandler handler;
+
+    public static APIs.Processor processor;
+
+    public static void main(String[] args) {
+        try {
+            handler = new APIsHandler();
+            processor = new APIs.Processor(handler);
+
+            Runnable simple = new Runnable() {
+                public void run() {
+                    simple(processor);
+                }
+            };
+            Runnable secure = new Runnable() {
+                public void run() {
+                    secure(processor);
+                }
+            };
+
+            new Thread(simple).start();
+            //TODO temporary commented out
+            //new Thread(secure).start();
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+    }
+
+    public static void simple(APIs.Processor processor) {
+        try {
+            TServerTransport serverTransport = new TServerSocket(9090);
+            TServer server = new TSimpleServer(new Args(serverTransport).processor(processor));
+
+            // Use this for a multithreaded server
+            // TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
+            System.out.println("Starting the simple server...");
+            server.serve();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void secure(APIs.Processor processor) {
+        try {
+            /*
+             * Use TSSLTransportParameters to setup the required SSL parameters. In this example
+             * we are setting the keystore and the keystore password. Other things like algorithms,
+             * cipher suites, client auth etc can be set. 
+             */
+            TSSLTransportParameters params = new TSSLTransportParameters();
+            // The Keystore contains the private key
+            params.setKeyStore("../../lib/java/test/.keystore", "thrift", null, null);
+
+            /*
+             * Use any of the TSSLTransportFactory to get a server transport with the appropriate
+             * SSL configuration. You can use the default settings if properties are set in the command line.
+             * Ex: -Djavax.net.ssl.keyStore=.keystore and -Djavax.net.ssl.keyStorePassword=thrift
+             * 
+             * Note: You need not explicitly call open(). The underlying server socket is bound on return
+             * from the factory class. 
+             */
+            TServerTransport serverTransport = TSSLTransportFactory.getServerSocket(9091, 0, null, params);
+            TServer server = new TSimpleServer(new Args(serverTransport).processor(processor));
+
+            // Use this for a multi threaded server
+            // TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
+            System.out.println("Starting the secure server...");
+            server.serve();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
