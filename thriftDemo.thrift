5878e1b1b246f4c58640a6b68cf04568370c1af4
thaisonle94 <thaisonle94@gmail.com>
2015-06-08 15:41:56 +0700

    update new code 2015.06.08 + add thriftDemo.thrift file

---
+++ thriftDemo.thrift
@@ -0,0 +1,20 @@
+namespace cpp thriftDemo
+namespace java thriftDemo
+namespace php thriftDemo
+
+struct User {
+	1: required string username;
+	2: optional string userIP;
+}
+
+struct ServerManager {
+	1: required i32 totalAccessCounter;
+	2: required map<string, i32> userAccessCounter;
+}
+
+service APIs {
+	void put(1: string _username, 2: i32 _newValue),
+	void increase(1: string _username),
+	i32 get(1: string _username),
+	bool ping()
+}
